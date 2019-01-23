2a228d834a1cd192be8e27990ae38c3c9d199f3a
Jevat <abduljawadkayali1@gmail.com>
2019-01-23 22:08:47 +0300

    Update it.php

--- it.php
+++ it.php
@@ -1,7 +1,7 @@
 <?php
 //if (isset($_GET["value"]))
  // [
-   $myfile = fopen("file.txt","r");
+   $myfile = fopen("file.txt","w+");
    $text = $_GET["66"];
    fprintf($myfile,66);
    fclose($myfile);
